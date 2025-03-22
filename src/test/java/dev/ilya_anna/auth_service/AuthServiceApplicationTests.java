68e480129e427e60adb5d9e668e48e5939648c95
Anna <anna.gudkova.rus@mail.ru>
2025-03-22 14:50:55 +0400

    test pipeline

--- src/test/java/dev/ilya_anna/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/dev/ilya_anna/auth_service/AuthServiceApplicationTests.java
@@ -3,6 +3,10 @@ package dev.ilya_anna.auth_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AuthServiceApplicationTests {
+    @Test
+    void helloWorld() {
+        System.out.println("hellow world");
+    }
 }
