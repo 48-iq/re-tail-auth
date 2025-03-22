b8827139a8770bbb1b2f6b476e8f6526e53145b0
48-iq <ivn25ov@mail.ru>
2025-03-22 13:36:06 +0400

    init

---
+++ src/test/java/dev/ilya_anna/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package dev.ilya_anna.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+}
