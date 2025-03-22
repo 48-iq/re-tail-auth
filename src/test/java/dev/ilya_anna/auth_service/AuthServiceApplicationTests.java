5558aecfd2c91854e012dc941e2dd6a11cb647e7
48-iq <ivn25ov@mail.ru>
2025-03-22 20:08:13 +0400

    save progress

--- src/test/java/dev/ilya_anna/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/dev/ilya_anna/auth_service/AuthServiceApplicationTests.java
@@ -1,8 +1,9 @@
 package dev.ilya_anna.auth_service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AuthServiceApplicationTests {
 }
